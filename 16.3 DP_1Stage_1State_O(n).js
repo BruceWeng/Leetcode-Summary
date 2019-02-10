2a76de8d7223dad91f55af8a81ac1de34f319458
Bruce Weng <weng8916@gmail.com>
2019-02-09 21:26:00 -0800

    16.3 DP_1Stage_1State_O(n).js

---
+++ 16.3 DP_1Stage_1State_O(n).js
@@ -0,0 +1,16 @@
+/**
+ * Definition
+ * 1. Each Stage only have 1 state
+ * 2. Each Stage only depends on O(n) prev states
+ */ 
+// Leetcode 300. Longest Increasing Subsequence(LIS)
+// Leetcode 139. Word Break
+
+/**
+ * More Practice
+ */
+/**
+ * Leetcode 96. Unique Binary Search Trees
+ * Leetcode 279. Perfect Squares
+ * Leetcode 32. Longest Valid Parentheses
+ */
